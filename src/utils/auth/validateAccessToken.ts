ee76fd67f033889f6b6c227680ff88ba8d76e7da
JuanDaGue <dj.jd2009@hotmail.com>
2024-12-02 09:10:09 -0500

    try again

--- src/utils/auth/validateAccessToken.ts
+++ src/utils/auth/validateAccessToken.ts
@@ -2,12 +2,26 @@ import { GraphQLClientSingleton } from 'app/graphql'
 import { customerName } from 'app/graphql/queries/customerName'
 import { cookies } from 'next/headers'
 
-export const validateAccessToken = async () => {
-    const cookieStore = cookies()
-    const accessToken = cookieStore.get('accessToken')?.value
-    const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
-    const { customer } = await graphqlClient.request(customerName, {
-        customerAccessToken: accessToken
-    })
-    return customer
+interface CustomerResponse {
+    customer: {
+        id: string;
+        name: string;
+        email: string;
+        // Add other fields as necessary
+    };
 }
+
+export const validateAccessToken = async (): Promise<CustomerResponse['customer'] | null> => {
+    try {
+        const cookieStore = cookies();
+        const accessToken = cookieStore.get('accessToken')?.value;
+        const graphqlClient = GraphQLClientSingleton.getInstance().getClient();
+        const response: CustomerResponse = await graphqlClient.request(customerName, {
+            customerAccessToken: accessToken,
+        });
+        return response.customer;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+};
