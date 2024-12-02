40904026b8a553ca953e1f2127fdb59f889e3055
JuanDaGue <dj.jd2009@hotmail.com>
2024-12-02 00:09:02 -0500

    try again

--- src/components/login/LoginForm/LoginForm.tsx
+++ src/components/login/LoginForm/LoginForm.tsx
@@ -1,23 +1,24 @@
-"use client"
+"use client";
+import { FormEvent } from "react";
 import { handleLogin } from "app/actions";
 import styles from "./LoginForm.module.sass";
 
 export const LoginForm = () => {
 
-const handleSubmit = async (event) => {
-    const formData = new FormData(event.target);
-    event.preventDefault();
-    await handleLogin(formData);
-}
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        await handleLogin(formData);
+    };
 
-return (
-        <div className={styles.NewAccountForm}  >
-        <h1 className={styles.NewAccountForm__title}>Login</h1>
-        <form onSubmit={handleSubmit} className={styles.NewAccountForm__form}>
-            <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
-            <input type="password" name="password" placeholder="password" />
-            <input type="submit" name="submit" value="Login" />
-        </form>
+    return (
+        <div className={styles.NewAccountForm} >
+            <h1 className={styles.NewAccountForm__title}>Login</h1>
+            <form onSubmit={handleSubmit} className={styles.NewAccountForm__form}>
+                <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
+                <input type="password" name="password" placeholder="password" />
+                <input type="submit" name="submit" value="Login" />
+            </form>
         </div>
     );
 }
