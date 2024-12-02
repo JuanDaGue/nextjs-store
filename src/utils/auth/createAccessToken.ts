ae80fe94606d403c40481a89a887ad8001ac7207
JuanDaGue <dj.jd2009@hotmail.com>
2024-12-02 09:17:59 -0500

    finaly

--- src/utils/auth/createAccessToken.ts
+++ src/utils/auth/createAccessToken.ts
@@ -1,25 +1,33 @@
-import { customerAccessTokenCreateMutation } from "app/graphql/mutations/customerAccessTokenCreate";
-import { GraphQLClient } from "graphql-request"
-import { cookies } from "next/headers";
+import { GraphQLClientSingleton } from "app/graphql"
+import { customerAccessTokenCreateMutation } from "app/graphql/mutations/customerAccessTokenCreate"
+import { cookies } from 'next/headers'
 
-
-export const createAccessToken = async (email: string , password: string)=>{
-    const cookiesStore =cookies();
-    const graphqlClient =GraphQLClient.getInstance().getClient();
-    const {customerAccessTokenCreate}  = await graphqlClient.request(customerAccessTokenCreateMutation , {
+export const createAccessToken = async (email: string, password: string) => {
+    const cookiesStore = cookies()
+    const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+    const { customerAccessTokenCreate }: {
+        customerAccessTokenCreate: {
+        customerAccessToken: {
+            accessToken: string
+            expiresAt: string
+        }
+        }
+    }
+        = await graphqlClient.request(customerAccessTokenCreateMutation, {
         "email": email,
         "password": password
-    });
-    const {accessToken, expiresAt} = customerAccessTokenCreate.customerAccessToken;
-
-    if(accessToken){
-        cookiesStore.set("accessToken", accessToken, {
-            path: "/",
-            expires: new Date(expiresAt),
-            httpOnly: true,
-            sameSite: "strict",
         })
-        return  accessToken;
 
+    const { accessToken, expiresAt } = customerAccessTokenCreate?.customerAccessToken
+
+    if (accessToken) {
+        cookiesStore.set("accessToken", accessToken, {
+        path: "/",
+        expires: new Date(expiresAt),
+        httpOnly: true,
+        sameSite: "strict"
+        })
+
+        return accessToken
     }
 }
